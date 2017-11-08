7045ddbede787e5910ba2c82aed463938036bf90
yh_gao <stayli117@163.com>
2017-11-08 17:18:28 +0800

    Signed-off-by: yh_gao <stayli117@163.com>

--- windowalert/src/main/java/net/people/windowalert/MainActivity.java
+++ windowalert/src/main/java/net/people/windowalert/MainActivity.java
@@ -3,6 +3,9 @@ package net.people.windowalert;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * android 悬浮窗测试
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
